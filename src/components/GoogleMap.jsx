59e2b6800a6f31a8345194ad202b7820fbc00a2b
sankar110000 <skaibalya748@gmail.com>
2025-04-17 02:33:37 +0530

    new changes are done

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -8,7 +8,7 @@ const GoogleMap = () => {
       const existingScript = document.getElementById("googleMaps");
       if (!existingScript) {
         const script = document.createElement("script");
-        script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap`;
+        script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDDAXDfO2VORRlJbXLu5CTstB25_xn4eao&callback=initMap`;
         script.id = "googleMaps";
         script.async = true;
         script.defer = true;
@@ -23,10 +23,10 @@ const GoogleMap = () => {
       if (!window.google || !mapRef.current) return;
       const location = { lat: 20.2961, lng: 85.8245 };
       const map = new window.google.maps.Map(mapRef.current, {
-        zoom: 10,
+        zoom: 5,
         center: location,
       });
-      new window.google.maps.Marker({
+      new window.google.maps.marker.AdvancedMarkerElement({
         position: location,
         map: map,
       });
@@ -37,10 +37,9 @@ const GoogleMap = () => {
 
   return (
     <div className="p-4">
-      <h3 className="text-xl font-semibold mb-4">My Google Map</h3>
       <div
         ref={mapRef}
-        className="w-full h-[400px] rounded-2xl shadow-md border"
+        className="w-full h-72 rounded-2xl shadow-md border"
       />
     </div>
   );
